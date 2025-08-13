caab335c4d5640d205e17115f4e5e0373e47ab4b
Felix Trummler <felix.trummler@student.tugraz.at>
2025-08-13 13:17:09 +0200

    plotting frame in GUI

--- GUI.py
+++ GUI.py
@@ -1,4 +1,7 @@
 import customtkinter as ctk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
 
 # Theme-Einstellungen
 ctk.set_appearance_mode("dark")  # "light" oder "dark"
@@ -73,7 +76,46 @@ class MyRadiobuttonFrame(ctk.CTkFrame):
     def set(self, value):
         self.variable.set(value)
 ################################################################################
-class PlottingFrame(ctk.CTk)
+class PlottingFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.label = ctk.CTkLabel(self, text="Sensor data")
+        self.label.grid(row=0, column=0, padx=20, pady=(5, 0))
+
+        # Matplotlib Figure erstellen
+        self.fig, self.ax = plt.subplots(figsize=(12, 9))
+        self.ax.set_title("Messwerte")
+        self.ax.set_xlabel("Zeit")
+        self.ax.set_ylabel("Wert")
+
+        # Canvas in Tkinter einbetten
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas_widget = self.canvas.get_tk_widget()
+        self.canvas_widget.grid(row=1, column=0, padx=10, pady=10)
+
+    def update_plot(self, x_data, y_data):
+        """Plot mit neuen Daten aktualisieren."""
+        self.ax.clear()
+        self.ax.plot(x_data, y_data, marker="o", color="cyan")
+        self.ax.set_title("Messwerte")
+        self.ax.set_xlabel("Zeit")
+        self.ax.set_ylabel("Wert")
+        self.canvas.draw()
+################################################################################
+# TabView
+class MyTabView(ctk.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        # create tabs
+        self.add("tab 1")
+        self.add("tab 2")
+
+        # add widgets on tabs
+        self.label = ctk.CTkLabel(master=self.tab("tab 1"))
+        self.label.grid(row=0, column=0, padx=20, pady=10)
+    
 
 ################################################################################
 #
@@ -85,23 +127,31 @@ class Gui(ctk.CTk):
         super().__init__()
 
         self.title("my GUI")
-        self.geometry("400x220")
-        self.grid_columnconfigure(0, weight=1)
+        self.geometry("900x500")
+        self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure(0, weight=1)
 
         values = ["value 1", "value 2", "value 3", "value 4", "value 5", "value 6"]
         self.scrollable_checkbox_frame = MyScrollableCheckboxFrame(self, title="Values", values=values)
-        self.scrollable_checkbox_frame.grid(row=0, column=0, padx=5, pady=(10, 0), sticky="nsew")
+        self.scrollable_checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
 
-        self.radiobutton_frame = MyRadiobuttonFrame(self,"Options",["option 1","option 2","option 3"])
-        self.radiobutton_frame.grid(row=0,column=1,padx=10,pady=(10,0),sticky="nsew")
+        self.radiobutton_frame = MyRadiobuttonFrame(self, "Options", ["option 1", "option 2", "option 3"])
+        self.radiobutton_frame.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="nsew")
 
-        self.button = ctk.CTkButton(self, text="my button", command=self.button_callback)
-        self.button.grid(row=3, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+        self.data_visualization = PlottingFrame(master=self)
+        self.data_visualization.grid(row=1, column=0, padx=20, pady=(20, 0), sticky="nsew")
+
+        self.button = ctk.CTkButton(self, text="Plot aktualisieren", command=self.button_callback)
+        self.button.grid(row=2, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
     def button_callback(self):
         print("checkbox_frame:", self.scrollable_checkbox_frame.get())
         print("radiobutton_frame:", self.radiobutton_frame.get())
+
+        # Beispiel: neue "Messwerte" simulieren
+        x = np.linspace(0, 10, 11)
+        y = np.random.rand(11) * 10
+        self.data_visualization.update_plot(x, y)
 
 gui = Gui()
 gui.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Flow_sensor
+# **Flow_sensor**
+
+![Sensor](https://cdn.thomasnet.com/insights-images/embedded-images/3a2f0ebf-bb3c-4d73-bb4b-a6e41d1eaf18/5c4e419f-9857-4a75-9b85-478341d057be/FullHD/shutterstock_531505201-min.jpg)
+
+## Short project description
+
+## Physics and Mechanics
+
+Sensoric principle ... Hall-effect (passive) or MID sensor (active) driven by change in Magnetic Flux density
+
+
+## Setup and electronics
+
+## Software and GUI
